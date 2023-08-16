cbda1f1a30dd6889fa5e39ec7f11cbd8718f84ee
Ariel Galindo <arielgalindo77@gmail.com>
2023-08-16 15:52:04 -0700

    First Commit

---
+++ dist/main.e80dc32f98d375dbc3d8.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatejsx"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ \"./node_modules/react-dom/client.js\");\n// 1) Import the React and ReactDOM libraries\n\n\n\n// 2) Get a reference to the div with ID root\nvar el = document.getElementById('root');\n\n// 3) Tell Reaact to take control of that element\nvar root = react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot(el);\n\n// 4) Create a component\nfunction App() {\n  var message = 'Bye there!';\n  if (Math.random() > 0.5) {\n    message = 'Hello there!';\n  }\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h1\", null, message);\n}\n\n// 5) Show the component on the screen\nroot.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(App, null));\n\n//# sourceURL=webpack://jsx/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("ea81ea371264b6352d5f")
+/******/ })();
+/******/ 
+/******/ }
+);
